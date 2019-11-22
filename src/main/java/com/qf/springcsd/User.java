b4215ee6c847a67a3c4f10d72ad66d1fd1e026eb
zhuyj1530 <434618542@qq.com>
2019-11-22 16:50:15 +0800

    Initial commit

---
+++ src/main/java/com/qf/springcsd/User.java
@@ -0,0 +1,10 @@
+package com.qf.springcsd;
+class Info{
+    int i = 1;
+}
+public class User {
+    public static void main(String[] args) {
+        Info info = new Info();
+        System.out.println(info.i);
+    }
+}
